453bcc209af66996f2a6f98cbb51466e789e5121
David Rodrigues <david.proweb@gmail.com>
2015-08-28 18:08:04 -0300

    Reformatting code;

--- src/VanillaFormatter/Formatter.php
+++ src/VanillaFormatter/Formatter.php
@@ -2,6 +2,10 @@
 
 namespace Rentalhost\VanillaFormatter;
 
+/**
+ * Class Formatter
+ * @package Rentalhost\VanillaFormatter
+ */
 class Formatter
 {
     /**
@@ -12,8 +16,10 @@ class Formatter
 
     /**
      * Normalize a value.
+     *
      * @param  string $value Value to normalize.
      * @param  string $type  Normalization type.
+     *
      * @return string
      */
     public static function normalize($value, $type = null)
@@ -21,10 +27,10 @@ class Formatter
         $type = strtolower($type);
 
         // Default expression will remove all not-numbers.
-        $replaceExpression = "\d";
+        $replaceExpression = '\d';
 
         // Phone types will not exclude "+" character.
-        if ($type === "phone") {
+        if ($type === 'phone') {
             $replaceExpression = "\\d+";
         }
 
@@ -34,9 +40,11 @@ class Formatter
 
     /**
      * Format a value.
+     *
      * @param  string $value  Value to format.
      * @param  string $type   Formatter type.
      * @param  string $region Formatter region.
+     *
      * @return string
      */
     public static function format($value, $type = null, $region = null)
@@ -49,12 +57,12 @@ class Formatter
             return $value;
         }
 
-        $matchedFormatNormalized = preg_replace("/\d/", "#", $matchedFormat["format"]);
+        $matchedFormatNormalized = preg_replace('/\d/', '#', $matchedFormat['format']);
         $valueNormalizedLength = strlen($valueNormalized);
 
         // Replace all "#" characters with normalized value.
         for ($i = 0; $i < $valueNormalizedLength; $i++) {
-            $matchedFormatNormalized = preg_replace("/#/", $valueNormalized[$i], $matchedFormatNormalized, 1);
+            $matchedFormatNormalized = preg_replace('/#/', $valueNormalized[$i], $matchedFormatNormalized, 1);
         }
 
         return $matchedFormatNormalized;
@@ -72,25 +80,27 @@ class Formatter
         }
 
         // Load formats and return.
-        return self::$formats = json_decode(file_get_contents(__DIR__ . "/../../res/formats.json"), true);
+        return self::$formats = json_decode(file_get_contents(__DIR__ . '/../../res/formats.json'), true);
     }
 
     /**
      * Match formats by type and region.
+     *
      * @param  string $value  Normalized value to format.
      * @param  string $type   Format type.
      * @param  string $region Format region.
+     *
      * @return array|null Matched format.
      */
     private static function getCompatibleFormat($value, $type, $region)
     {
         foreach (self::getFormats() as $format) {
-            if ($format["type"] !== $type) {
+            if ($format['type'] !== $type) {
                 // Filter by type.
                 continue;
             }
 
-            if ($format["length"] !== strlen($value)) {
+            if ($format['length'] !== strlen($value)) {
                 // Filter by length.
                 continue;
             }
@@ -98,24 +108,29 @@ class Formatter
             // Filter by region.
             if ($region === null) {
                 // 1. If region was not defined, so ignore all formats with region.
-                if (isset($format["region"])) {
+                if (array_key_exists('region', $format)) {
                     continue;
                 }
-            } else {
+            }
+            else {
                 // 2. If region was defined, so ignore all format without region.
                 // 3. If region was defined, so ignore formats with differents regions.
-                if (!isset($format["region"]) || $format["region"] !== $region) {
+                if (!array_key_exists('region', $format) ||
+                    $format['region'] !== $region
+                ) {
                     continue;
                 }
             }
 
             // Normalize format to regular expression.
-            $formatExpression = preg_replace([ "/[^\d#]/", "/#/" ], [ null, '\d' ], $format["format"]);
+            $formatExpression = preg_replace([ '/[^\d#]/', '/#/' ], [ null, '\d' ], $format['format']);
 
             // If match format expression, so return matched format.
             if (preg_match("/^{$formatExpression}$/", $value)) {
                 return $format;
             }
         }
+
+        return null;
     }
 }
